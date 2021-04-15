7256c90d1708c0cde52f8418f82ae11fc70b0803
matsjoonas <mj@codelight.eu>
2021-04-15 20:23:16 +0300

    Add app.php because Application.php will not run for whatever reason

---
+++ appinfo/app.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace OCA\ElectronicSignatures\AppInfo;
+
+use OCP\AppFramework\App;
+use OCP\Util;
+
+class Application extends App {
+
+    public function __construct() {
+        parent::__construct('electronicsignatures');
+        Util::addScript('electronicsignatures', '../js-static/index');
+    }
+}
