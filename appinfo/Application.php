6a51b421d6a7a7b9a03802fcc4696def7951f2be
Tõnis Palts <tonis@maagiline.ee>
2021-04-22 09:50:50 +0300

    Check if Application class exists

--- appinfo/Application.php
+++ appinfo/Application.php
@@ -6,10 +6,13 @@ namespace OCA\ElectronicSignatures\AppInfo;
 
 use OCP\AppFramework\App;
 
-class Application extends App {
-
-	public function __construct() {
-		parent::__construct('electronicsignatures');
-        AppShared::registerDependencies();
-	}
+// Check if class exists, because app.php and Application.php behave unexpectedly in different environments. app.php
+// is supposed to be deprecated, but on one of our setups, it was required. On another setup, it did not work.
+if (!class_exists(Application::class)) {
+    class Application extends App {
+        public function __construct() {
+            parent::__construct('electronicsignatures');
+            AppShared::registerDependencies();
+        }
+    }
 }
--- appinfo/app.php
+++ appinfo/app.php
@@ -6,10 +6,12 @@ namespace OCA\ElectronicSignatures\AppInfo;
 
 use OCP\AppFramework\App;
 
-class Application extends App {
-
-    public function __construct() {
-        parent::__construct('electronicsignatures');
-        AppShared::registerDependencies();
+// For explanation, refer to comments in Application.php
+if (!class_exists(Application::class)) {
+    class Application extends App {
+        public function __construct() {
+            parent::__construct('electronicsignatures');
+            AppShared::registerDependencies();
+        }
     }
 }
