a09e8c07be4be4e215fc6b0915d9184c77b1ab33
matsjoonas <mj@codelight.eu>
2021-06-08 14:52:13 +0300

    feat: add demo method buttons

--- src/config.js
+++ src/config.js
@@ -1,6 +1,56 @@
 const config = {
   appId: 'electronicsignatures',
   features: {},
+  methods: [
+    {
+      name: 'idCardEE',
+      icon: 'eid_idkaart_mark.png',
+      text: 'ID Kaart',
+      enabled: true,
+    },
+    {
+      name: 'mobileIdEE',
+      icon: 'eid_mobiilid_mark.png',
+      text: 'Mobiil ID',
+      enabled: true,
+    },
+    {
+      name: 'smartId',
+      icon: 'smart-id-inline-logo.svg',
+      text: 'Smart-ID',
+      enabled: true,
+    },
+    {
+      name: 'eParakstsID',
+      icon: 'latvia-id-card.png',
+      text: 'eParaksts ID',
+      enabled: true,
+    },
+    {
+      name: 'eParakstsMobile',
+      icon: 'eparaksts-mobile.png',
+      text: 'eParaksts Mobile',
+      enabled: true,
+    },
+    {
+      name: 'idCardLT',
+      icon: '',
+      text: 'AT kortelė',
+      enabled: true,
+    },
+    {
+      name: 'mobileIdLT',
+      icon: '',
+      text: 'M. parašas',
+      enabled: true,
+    },
+    {
+      name: 'belgianId',
+      icon: 'belgia-id-card.svg',
+      text: 'Belgian ID card',
+      enabled: true,
+    },
+  ],
 };
 
 export default config;
--- webpack.js
+++ webpack.js
@@ -1,3 +1,35 @@
-const webpackConfig = require('@nextcloud/webpack-vue-config')
+const webpackConfig = require('@nextcloud/webpack-vue-config');
+webpackConfig.module.rules = [
+  {
+    test: /\.css$/,
+    use: ['style-loader', 'css-loader'],
+  },
+  {
+    test: /\.scss$/,
+    use: ['style-loader', 'css-loader', 'sass-loader'],
+  },
+  {
+    test: /\.(js|vue)$/,
+    use: 'eslint-loader',
+    exclude: /node_modules/,
+    enforce: 'pre',
+  },
+  {
+    test: /\.vue$/,
+    loader: 'vue-loader',
+  },
+  {
+    test: /\.js$/,
+    loader: 'babel-loader',
+    exclude: /node_modules/,
+  },
+  {
+    test: /\.(png|jpg|gif|svg)$/,
+    loader: 'url-loader',
+    options: {
+      esModule: false,
+    }
+  },
+];
 
 module.exports = webpackConfig
