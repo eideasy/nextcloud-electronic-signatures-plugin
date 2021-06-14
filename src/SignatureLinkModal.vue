4631279139c3ac554fd9bf464b04df03458f20e1
matsjoonas <mj@codelight.eu>
2021-06-14 15:06:38 +0300

    feat(containerSelect) implement container select

--- src/SignatureLinkModal.vue
+++ src/SignatureLinkModal.vue
@@ -6,6 +6,15 @@ import { generateUrl } from '@nextcloud/router';
 import queryString from 'query-string';
 import OC from './OC';
 
+const CONTAINER_TYPE = {
+  asice: 'asice',
+  pdf: 'pdf',
+};
+
+const getFileExtension = function getFileExtension(filename) {
+  return filename.split('.').pop();
+};
+
 export default {
   name: 'SignatureLinkModal',
   components: {
@@ -18,13 +27,43 @@ export default {
       successMessage: null,
       isLoading: false,
       adminSettings: null,
+      containerType: '',
+      containerTypeOptions: [
+        {
+          value: CONTAINER_TYPE.asice,
+          text: '.asice',
+        },
+        {
+          value: CONTAINER_TYPE.pdf,
+          text: '.pdf',
+        },
+      ],
       email: '',
       filename: '',
     };
   },
   computed: {
     fileExtension() {
-      return this.filename.split('.').pop();
+      return getFileExtension(this.filename);
+    },
+    containerTypeModel: {
+      get() {
+        if (getFileExtension(this.filename) !== CONTAINER_TYPE.pdf) {
+          return CONTAINER_TYPE.asice;
+        }
+
+        if (this.containerType) {
+          return this.containerType;
+        } else {
+          return CONTAINER_TYPE.pdf;
+        }
+      },
+      set(val) {
+        this.containerType = val;
+      },
+    },
+    shouldShowContainerSelect() {
+      return this.fileExtension === CONTAINER_TYPE.pdf;
     },
   },
   mounted() {
@@ -36,7 +75,7 @@ export default {
   },
   methods: {
     generateNextcloudUrl(url) {
-      return generateUrl(url);
+	return generateUrl(url);
     },
     showModal() {
       if (!this.adminSettings) {
@@ -101,6 +140,7 @@ export default {
         data: {
           path: this.getFilePath(),
           email: this.email,
+          container_type: this.containerTypeModel,
         },
       })
         .then(function() {
@@ -151,6 +191,25 @@ export default {
           <form
               action=""
               @submit.prevent="onSubmit">
+            <div v-if="shouldShowContainerSelect">
+              <label
+                  class="label"
+                  for="containerType">
+                {{ $t($globalConfig.appId, 'Container type') }}
+              </label>
+              <div class="fieldRow">
+                <select
+                    id="containerType"
+                    v-model="containerTypeModel">
+                  <option
+                      v-for="option in containerTypeOptions"
+                      :key="option.value"
+                      :value="option.value">
+                    {{ option.text }}
+                  </option>
+                </select>
+              </div>
+            </div>
             <label
                 class="label"
                 for="signingLinkEmail">
