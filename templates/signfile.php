ad7a4c1d66878405989657070300d93ec5534939
matsjoonas <mj@codelight.eu>
2021-06-14 16:11:51 +0300

    Provide initial data

--- templates/signfile.php
+++ templates/signfile.php
@@ -1,1 +1,12 @@
-<div id="electronic-signatures-sign-root"></div>
+<?php
+/** @var array $_ */
+/** @var \OCP\IL10N $l */
+?>
+
+<div
+        id="electronic-signatures-sign-root"
+        data-doc-id="<?php echo $_['doc_id'] ?>"
+        data-mime-type="<?php echo $_['file_mime_type'] ?>"
+        data-file-content="<?php echo $_['file_content'] ?>"
+        data-file-name="<?php echo $_['file_name'] ?>"
+></div>
