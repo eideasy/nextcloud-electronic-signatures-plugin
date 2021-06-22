7a8182e0ef96b36601391e76d30196a0d68ba03b
matsjoonas <mj@codelight.eu>
2021-06-22 11:28:56 +0300

    Misc

--- src/main.js
+++ src/main.js
@@ -9,6 +9,12 @@ import config from './config';
 Vue.prototype.$t = translate;
 Vue.prototype.$n = translatePlural;
 Vue.prototype.$globalConfig = config;
+// from https://nextcloud-vue-components.netlify.app/
+// Some components require Nextcloud functionality to work, so it is currently recommended to extend Vue with the following:
+Vue.prototype.t = window.t;
+Vue.prototype.n = window.n;
+Vue.prototype.OC = window.OC;
+Vue.prototype.OCA = window.OCA;
 
 let vueInstance;
 if (document.getElementById('electronic-signatures-root')) {
