1bfb07870d15dec86f16df02a5d4be0ae5657ffc
Mats-Joonas Kulla <matsjoonas@gmail.com>
2021-06-30 14:28:02 +0300

    feat: style the signing success page (NA-94) (#45)
    * Style the success page
    
    * Wrap texts in translation funciton

--- templates/success.php
+++ templates/success.php
@@ -3,4 +3,24 @@
 /** @var \OCP\IL10N $l */
 ?>
 
-<h1>Signing was successful!</h1>
+<style>
+    .container {
+        width: 100%;
+        margin: 0 auto;
+        max-width: 980px;
+        padding: 0 20px;
+    }
+
+    .content {
+        text-align: center;
+        margin-top: 60px;
+        margin-bottom: 60px;
+    }
+</style>
+
+<div class="container">
+    <div class="content">
+        <h2><?php p($l->t('Thank you!')); ?></h2>
+        <h3><?php p($l->t('The signed file has been downloaded to your browser.')); ?></h3>
+    </div>
+</div>
