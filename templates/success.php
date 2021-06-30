f47540bd2d5b488f61b8063eea4ef7cecc554887
matsjoonas <mj@codelight.eu>
2021-06-30 14:24:31 +0300

    Style the success page

--- templates/success.php
+++ templates/success.php
@@ -3,4 +3,24 @@
 /** @var \OCP\IL10N $l */
 ?>
 
-<h1>Signing was successful!</h1>
+<style>
+    .container {
+        width: 100%;
+        margin: 0 auto;
+        max-width: 980px;
+        padding: 0 20px;
+    }
+
+    .content {
+        text-align: center;
+        margin-top: 60px;
+        margin-bottom: 60px;
+    }
+</style>
+
+<div class="container">
+    <div class="content">
+        <h2>Thank you!</h2>
+        <h3>The signed file has been downloaded to your browser.</h3>
+    </div>
+</div>
